90bf2800b82aeaf1aae0cc65a98ec48318a8ffc6
padmini v <padmini.v@perfios.com>
2021-11-01 06:30:03 +0530

    ADDED: Rest api to show name from query parameter

--- src/main/java/com/bridgelabz/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/controller/GreetingController.java
@@ -1,16 +1,27 @@
 package com.bridgelabz.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
 public class GreetingController 
 {
-	@RequestMapping(value = {"", "/", "/home"})
-	public String sayHello() 
+	@RequestMapping(value = { "", "/", "/home" })
+	public ResponseEntity<String> sayHello() 
 	{
-		return "Hello Everyone!";
+		return new ResponseEntity<String>("Hello everyone!", HttpStatus.OK);
 	}
 
+	
+	@RequestMapping(value = { "/query" })
+	public String sayHello(@RequestParam(value = "fname", defaultValue = "Padmini") String fname,
+			@RequestParam(value = "lname", defaultValue = "Sharma") String lname) 
+	{
+		return "Welcome " + fname + " " + lname + "!";
+	}
 }
