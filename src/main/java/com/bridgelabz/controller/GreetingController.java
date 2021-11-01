d25fda1d9bf6fea4b69d3d1cc9a14c4f008000b6
padmini v <padmini.v@perfios.com>
2021-11-01 06:28:21 +0530

    ADDED: gGreetingController class with function sayHello

---
+++ src/main/java/com/bridgelabz/controller/GreetingController.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class GreetingController 
+{
+	@RequestMapping(value = {"", "/", "/home"})
+	public String sayHello() 
+	{
+		return "Hello Everyone!";
+	}
+
+}
