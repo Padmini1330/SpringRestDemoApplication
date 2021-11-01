1c935e85d63d80ee21be187767725394e6e39cfa
padmini v <padmini.v@perfios.com>
2021-11-01 06:35:53 +0530

    ADDED: Post method to pass firstname and lastname as query parameters

--- src/main/java/com/bridgelabz/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/controller/GreetingController.java
@@ -4,10 +4,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bridgelabz.model.User;
 
 @RestController
 @RequestMapping("/hello")
@@ -31,5 +35,11 @@ public class GreetingController
 	public String parameterName(@PathVariable String name) 
 	{
 		return "Hello " + name;
+	}
+	
+	@PostMapping("/post")
+	public String setUser(@RequestBody User name) 
+	{
+		return "Hello " + name.getFirstName() + " " + name.getLastName();
 	}
 }
