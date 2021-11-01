4eb295bce8496b5fc8129a874ef48cd686cc9e40
padmini v <padmini.v@perfios.com>
2021-11-01 06:36:17 +0530

    ADDED: User model class

---
+++ src/main/java/com/bridgelabz/model/User.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.model;
+
+public class User 
+{
+	private String firstName;
+	private String lastName;
+
+	public String getFirstName() 
+	{
+		return this.firstName;
+	}
+
+	public String getLastName() 
+	{
+		return this.lastName;
+	}
+}
